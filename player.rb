a57e6678dabb117b8aaacb1f69a0b7739c850c20
Javier Murillo <jhmmurillo@gmail.com>
2013-04-22 15:58:06 -0700

    Level 2 is complete and level 3 now appears on README

--- player.rb
+++ player.rb
@@ -1,5 +1,18 @@
 class Player
   def play_turn(warrior)
-    warrior.walk!
+    @warrior = warrior
+    if enemy?
+      warrior.attack!
+    elsif space?
+      warrior.walk!
+    end
+  end
+
+  def enemy?
+    @warrior.feel.enemy? ? true : false
+  end
+
+  def space?
+    @warrior.feel.empty? ? true : false
   end
 end
