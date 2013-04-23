a480509c90b186c5b8873f9d9267eec06502f205
Javier Murillo <jhmmurillo@gmail.com>
2013-04-22 18:45:52 -0700

    level 6 complete, the warrior can feel when is dying and escape for take health

--- player.rb
+++ player.rb
@@ -1,28 +1,39 @@
 class Player
   def play_turn(warrior)
     @health ||= warrior.health
+    @direction ||= :forward
     @warrior = warrior
+    @retreating ||= 0
+
+    change_direction if wall?
 
     if enemy?
-      warrior.attack!
+      warrior.attack!(@direction)
     elsif captive?
-      warrior.rescue!
+      warrior.rescue!(@direction)
+    elsif retreating?
+      @retreating -= 1
+      warrior.walk!(@direction)
+    elsif dying?
+      change_direction
+      @retreating = 2
+      warrior.walk!(@direction)
     elsif taking_damage?
-      warrior.walk!
+      warrior.walk!(@direction)
     elsif can_rest?
       warrior.rest!
     elsif space?
-      warrior.walk!
+      warrior.walk!(@direction)
     end
     @health = warrior.health
   end
 
   def enemy?
-    @warrior.feel.enemy? ? true : false
+    @warrior.feel(@direction).enemy? ? true : false
   end
 
   def space?
-    @warrior.feel.empty? ? true : false
+    @warrior.feel(@direction).empty? ? true : false
   end
 
   def can_rest?
@@ -33,7 +44,23 @@ class Player
     @warrior.health < @health ? true : false
   end
 
+  def dying?
+    @warrior.health < @health and @warrior.health < 10 ? true : false
+  end
+
   def captive?
-    @warrior.feel.captive? ? true : false
+    @warrior.feel(@direction).captive? ? true : false
+  end
+
+  def change_direction
+    @direction = @direction == :backward ? :forward : :backward
+  end
+
+  def retreating?
+    @retreating != 0 ? true : false
+  end
+
+  def wall?
+    @warrior.feel(@direction).wall? ? true : false
   end
 end
